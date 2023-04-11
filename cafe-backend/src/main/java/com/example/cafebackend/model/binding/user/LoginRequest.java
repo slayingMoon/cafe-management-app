56d7cb5fbf2acfce0ad2f076c4eb8cf45e1d8a2d
Georgi Kolev <georgi.kolev@nevexis.com>
2023-04-11 19:10:48 +0300

    added lombok annotations for allArgs/noArgs constructors on LoginRequest and Product entity

--- cafe-backend/src/main/java/com/example/cafebackend/model/binding/user/LoginRequest.java
+++ cafe-backend/src/main/java/com/example/cafebackend/model/binding/user/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.example.cafebackend.model.binding.user;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 
     @NotNull
--- cafe-backend/src/main/java/com/example/cafebackend/model/entity/Product.java
+++ cafe-backend/src/main/java/com/example/cafebackend/model/entity/Product.java
@@ -1,11 +1,15 @@
 package com.example.cafebackend.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data //generates getters, setters and constructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "products")
 public class Product {
